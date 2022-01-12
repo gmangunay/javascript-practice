b2832f8a596c4c8531d340e7cb4c8f7f57f6e0d6
Genesis Mangunay <genesis.mangunay7@gmail.com>
2022-01-11 20:48:20 -0800

    take home assignment

--- Day1/js/script.js
+++ Day1/js/script.js
@@ -9,8 +9,8 @@
 multi line comment
 
 */
-let x = "Hi ny name is Genesis Mangunay";
-let y = "My favorite food is Oreos";
+let x = "Hi ny name is Genesis Mangunay!!!";
+let y = "My favorite food is Oreos!!!";
 
 console.log(x);
 console.log(y);

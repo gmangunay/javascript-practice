08ed697216a8abb57007ee9388043f407d6f02d1
Genesis Mangunay <genesis.mangunay7@gmail.com>
2022-01-13 18:56:36 -0800

    practice

---
+++ Day 1/js/script.js
@@ -0,0 +1,16 @@
+
+//single line comment
+
+/*
+
+multi line comment
+
+*/
+let x = "Hi ny name is Genesis Mangunay!!!";
+let y = "My favorite food is Oreos!!!";
+
+console.log(x);
+console.log(y);
+
+//console.log( "Genesis Mangunay");
+//console.log( "My favorite food is Oreos");

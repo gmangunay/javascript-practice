833dbf6d9db25e2b6e9372fa934e546eac054e92
Genesis Mangunay <genesis.mangunay7@gmail.com>
2022-02-01 18:47:44 -0800

    update

---
+++ day-7/js/script.js
@@ -0,0 +1,18 @@
+// //daily challenge
+// const regex = new RegExp(/^[a-zA-Z]/);
+
+// function validatePass(password){
+
+//     if(password.length < 6 || password.length > 20){
+//         alert("Password must contian between 6 and 20 characters");
+//     }
+//     else if(!regex.test(password)){
+//         alert("Password must start with a letter");
+//     }
+//     else{
+//         alert("Password is ok!");
+//     }
+// }
+
+// const userPassword = prompt("Please eneter a password");
+// validatePass(userPassword);
